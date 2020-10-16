3a9bbb36ce69dedcea620dca714bb10b7975f193
ines Chebil <ines.chebil26@gmail.com>
2020-10-16 10:04:05 +0200

    Display Json Objects on a certain url

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const http = require("http");
 const products = require("./data/products.json");
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify(products));
+  if (req.url === "/api/products") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(products));
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ Message: "Route Not Found" }));
+  }
 });
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
