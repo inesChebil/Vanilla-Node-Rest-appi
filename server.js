b391f86fffe3a918afb8342015f61fded69a3919
ines Chebil <ines.chebil26@gmail.com>
2020-10-16 09:54:24 +0200

    Display Json Objects

---
+++ server.js
@@ -0,0 +1,8 @@
+const http = require("http");
+const products = require("./data/products.json");
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify(products));
+});
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
